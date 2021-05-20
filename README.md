8b6f3d9fd6bd44d7374a2e14f83fbecbfb55f5c2
taiseen <taiseen.cse@gmail.com>
2021-05-20 15:47:52 +0600

    Conditional CSS Border ==> OK

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Note-app-Material-ui | React App <|> [Live Link](https://www.fb.com) 
+# Note-app-Material-ui | React App <|> [Live Link](https://note-app-material-ui.netlify.app) 
 
 ## Topic of Focus:
 - JSON Server use here for API calling
+- Array of Menu & useHistory for click event
 
 
 ## Technologies :
--- src/Components/NoteCard.js
+++ src/Components/NoteCard.js
@@ -1,14 +1,37 @@
-import { Card, CardContent, CardHeader, IconButton, Typography } from '@material-ui/core';
+import { Card, CardContent, CardHeader, IconButton, Typography, makeStyles } from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
 import React from 'react';
 
+const width = 1;
+const style = 'solid'; //  [ solid, dashed , dotted ]
+
+const useStyles = makeStyles({
+    test: {
+        border: (note) => {
+            if (note.category === 'work') {
+                return `${width}px ${style} red`
+            } else if (note.category === 'money') {
+                return `${width}px ${style} green`
+            } else if (note.category === 'reminder') {
+                return `${width}px ${style} blue`
+            } else if (note.category === 'todo') {
+                return `${width}px ${style} orange`
+            }
+        }
+    },
+});
+
+
+
 function NoteCard({ note, handleDelete }) {
+
+    const classes = useStyles(note);
 
     const { id, title, category, details } = note;
 
     return (
 
-        <Card elevation={1}>
+        <Card elevation={1} className={classes.test}>
             <CardHeader
                 action={
                     <IconButton onClick={() => handleDelete(id)}>
@@ -18,7 +41,7 @@ function NoteCard({ note, handleDelete }) {
                 title={title}
                 subheader={category}
             />
-            
+
             <CardContent>
                 <Typography variant="body2" color="textSecondary">
                     {details}
